cd9f3cb2226df259c38b0f4dea99f1859e06f4a2
github username <github email address>
2019-10-08 14:17:42 -0400

    Add authenticity_token in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+
+    protect_from_forgery with: :exception  
+    #authenticity_token which prevents again CSRF (Cross Site Request Forgery).
+    
 end
