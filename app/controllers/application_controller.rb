447dbfcd1d45f3c7480f16d0e6cbeafbc9d237eb
github username <github email address>
2019-10-10 13:36:30 -0400

    Change port to 3001

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
 
-    protect_from_forgery with: :exception  
+    # protect_from_forgery with: :exception  
     #authenticity_token which prevents again CSRF (Cross Site Request Forgery).
     
 end
