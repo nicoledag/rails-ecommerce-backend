005f2cbf48570e6d8f84fdaefe6c8b4b20827033
github username <github email address>
2019-10-10 22:00:26 -0400

    Take out CSF token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::API
 
-    # protect_from_forgery with: :exception  
-    #authenticity_token which prevents again CSRF (Cross Site Request Forgery).
-    
 end
