58eb2811c98b6c2240e8f26a0f835d7983cad9b4
github username <github email address>
2019-10-10 22:01:41 -0400

    Add serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-    has_many :businesses
+    has_many :businesses, dependent: :destroy
 
     validates :username, :email, presence: true
     validates :username, :email, uniqueness: true
