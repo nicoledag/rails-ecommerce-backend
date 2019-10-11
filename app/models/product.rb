18d94f9faf1420b8755a6190ad0daae40f688495
github username <github email address>
2019-10-10 22:01:30 -0400

    Add serializer

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
 
     belongs_to :business
-    has_many :images
-    has_many :colors
+    has_many :images, dependent: :destroy
+    has_many :colors, dependent: :destroy
 
     validates :name, :description, :category, :subcategory, :price, :item_number, presence: true
     validates :item_number, uniqueness: true
