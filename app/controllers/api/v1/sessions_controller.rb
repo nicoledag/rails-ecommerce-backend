2a02a0632a87fbf1f57b4e7d4eeaa6e395bf4e57
github username <github email address>
2019-10-17 13:20:17 -0400

    Add create action for user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,20 @@ class API::V1::SessionsController < ApplicationController
 
 
     def create
+        @user = User.find_by(username: params[:session][:user][:username])
+
+        if @user && @user.authenticate(params[:session][:user][:password])
+            set_session
+
+            resp = { user: @user}
+            render json: resp, status: :ok
+        else
+            resp = {
+                error: "Invalid credentials",
+                details: @user.errors.full_messages
+            }
+            render json: resp, status: :unauthorized
+        end
     end
 
     def get_current_user
@@ -10,5 +24,10 @@ class API::V1::SessionsController < ApplicationController
     def destroy
     end
 
+    private
+
+    def set_session
+        session[:user_id] = @user.id
+    end
     
 end
