9ab173db24c621c0709b2d1352a267ac9b44ba98
github username <github email address>
2019-10-21 14:20:29 -0400

    Move has_secure_passwsord to top

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-
+    has_secure_password
     has_many :businesses, dependent: :destroy
 
     validates :username, :email, presence: true
     validates :username, :email, uniqueness: true
 
-    has_secure_password
+    
 
    
 end
