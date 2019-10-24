7418a372e1c771fea621f25b5edb111c083b7e45
github username <github email address>
2019-10-24 11:19:32 -0400

    Add serializer to show associated products

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,6 +1,22 @@
 class CategorySerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name 
+  attributes :id, :name
 
-  has_many :products
+
+  attribute :products do |category|
+    category.products.map do |product|
+      {
+        name: product.name,
+        description: product.description,
+        subcategory: product.subcategory.name,
+        subcategory: product.subcategory.id,
+        price: product.price,
+        item_number: product.item_number,
+        business_id: product.business_id
+      }
+    end
+  end
+
+  # has_many :products, serializer: ProductSerializer
+
 end
