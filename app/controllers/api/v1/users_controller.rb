41c689268167ecbbc718fd9d191b8df6dbb1de71
github username <github email address>
2019-10-24 11:19:52 -0400

    Add fast json serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,11 +2,7 @@ class API::V1::UsersController < ApplicationController
 
     def index
         @users = User.all 
-        # render json: @users
-
-        # json_string = MovieSerializer.new(movie).serialized_json
         users_json = UserSerializer.new(@users).serialized_json
-
         render json: users_json
     end
 
