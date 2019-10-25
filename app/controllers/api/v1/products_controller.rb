af196247dc4d12773600a0ab0475b57d4b0e1807
github username <github email address>
2019-10-25 10:09:26 -0400

    Add json fast serializtion to products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,10 @@ class API::V1::ProductsController < ApplicationController
 
     def index
         @products = Product.all 
-        render json: @products
+        # render json: @products
+
+        products_json = ProductSerializer.new(@products).serialized_json
+        render json: products_json
     end
 
 end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -3,10 +3,30 @@ class ProductSerializer
 
   attributes :id, :name, :description, :category, :subcategory, :price, :business_id, :item_number
 
-  has_many :images, dependent: :destroy
-  has_many :colors, dependent: :destroy
+
   belongs_to :business
   belongs_to :category
+
+  attribute :images do |product|
+    product.images.map do |image|
+      {
+        link: image.link,
+      }
+    end
+  end
+
+  attribute :colors do |product|
+    product.colors.map do |color|
+      {
+        color_name: color.color_name,
+        available_qty: color.available_qty
+      }
+    end
+  end
+
+
+  # has_many :images, dependent: :destroy
+  # has_many :colors, dependent: :destroy
 
 
 end
