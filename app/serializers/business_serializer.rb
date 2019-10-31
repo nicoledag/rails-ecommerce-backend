c40430fa5c9bc5521c44e8d101c8e30fd00e0b9a
github username <github email address>
2019-10-31 12:21:48 -0400

    Add products to business serializer

--- app/serializers/business_serializer.rb
+++ app/serializers/business_serializer.rb
@@ -3,5 +3,21 @@ class BusinessSerializer
   
   attributes :id, :name, :tax_id
 
-  has_many :products, dependent: :destroy
+  attribute :products do |business|
+    business.products.map do |product|
+      {
+        name: product.name,
+        description: product.description,
+        subcategory_name: product.subcategory.name,
+        subcategory_id: product.subcategory.id,
+        price: product.price,
+        item_number: product.item_number,
+        business_id: product.business_id,
+        image: product.image,
+        id: product.id
+      }
+    end
+  end
+
+  # has_many :products, dependent: :destroy
 end
