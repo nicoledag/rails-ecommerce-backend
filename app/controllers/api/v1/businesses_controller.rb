8fc7fc2ee6382ff42ac9ac8cd35e37a9826fd7b4
github username <github email address>
2019-11-06 14:03:51 -0500

    Work on business update method

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -1,5 +1,7 @@
 class API::V1::BusinessesController < ApplicationController
 
+    before_action :set_business, only: [:show, :edit, :update, :destroy]
+   
     def index 
         @businesses = current_user.businesses
         businesses_json = BusinessSerializer.new(@businesses).serialized_json
@@ -20,7 +22,40 @@ class API::V1::BusinessesController < ApplicationController
         end
     end
 
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update 
+
+        # binding.pry
+        if business_user_equals_current_user && @business.update(business_params)
+            render json:  BusinessSerializer.new(@business).serialized_json, status: :created
+          else
+            error_resp = {
+              error: @trip.errors.full_messages.to_sentence
+            }
+           render json: error_resp, status: :unprocessable_entity
+          end
+    end
+
     def business_params
         params.require(:business).permit(:name, :tax_id)
       end
+
+private
+
+    def set_business
+        @business = Business.find_by(id: params[:id])
+    end
+
+     def business_user_equals_current_user
+         @business.user == current_user
+     end
+
+
 end
